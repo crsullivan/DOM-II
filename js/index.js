268aeb745cdda15b79ed744ef7a682f6443638f2
Connor R. Sullivan <crsullivan@protonmail.com>
2019-09-03 12:52:38 -0700

    Nav Bar prevention

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const navBar = document.querySelectorAll('nav');
+const navLink = document.querySelectorAll('nav .nav-link');
+
+// navLink.addEventListener('click', e => {
+    
+  console.log(navBar);
+  console.log(navLink);
+
+  navLink[0].addEventListener('click', e => {console.log('link is clicked'); e.preventDefault();})
+  navLink[1].addEventListener('click', e => {console.log('link is clicked'); e.preventDefault();})
+  navLink[2].addEventListener('click', e => {console.log('link is clicked'); e.preventDefault();})
+  navLink[3].addEventListener('click', e => {console.log('link is clicked'); e.preventDefault();})
